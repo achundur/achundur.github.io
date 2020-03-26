8a2b1b0d390f2422f321c76357c033bbe906e75d
Adhiti Chundur <59855558+achundur@users.noreply.github.com>
2020-03-25 23:42:58 -0400

    /ljk

--- homework_6/main.js
+++ homework_6/main.js
@@ -69,7 +69,7 @@ function addItem() {
 
 
     var newOrder = new Order(flavorInputValue, glazeInputValue, amountInputValue);
-    console.log('tetst', newOrder)
+    console.log('test', newOrder)
     console.log(newOrder.addToCart());
 
 
@@ -92,20 +92,3 @@ function deleteItem(clickedObject){
 
 }
 
-
-
-// function deleteListItem(item) {
-//     // remove li element (item) from ol element (item.parentNode)
-//     item.parentNode.removeChild(item);
-//     }
-
-
-// function completeListItem(item) {
-//     if (item.checked) { // true if the input checkbox is checked
-//     item.parentNode.style.textDecoration = "line-through";
-//     // in css, this would be: "text-decoration: line-through"
-//     } else {
-//     item.parentNode.style.textDecoration = "none";
-//     // in css, this would be: "text-decoration: none"
-//     }
-//     }
