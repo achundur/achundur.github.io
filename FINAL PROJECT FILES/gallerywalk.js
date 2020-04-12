8fe115c9f86282aef294d6ef3a60c29579020ce2
Adhiti Chundur <59855558+achundur@users.noreply.github.com>
2020-04-12 14:56:35 -0400

    upudate 4/12

--- FINAL PROJECT FILES/gallerywalk.js
+++ FINAL PROJECT FILES/gallerywalk.js
@@ -10,7 +10,31 @@ $( document ).ready(function() {
 
 function onClick_LearnMore(event){
     var galleryItem=$(event.target).attr("data-adhiti");
-    alert(galleryItem);
+    console.log(galleryItem);
+    $("#gItemIntro").attr( "style", "display: none;" );
+    $("#gItemDetail").show(3000);
+    //$("#gItemDetail").fadeIn(3000,"linear");
+    /*
+    $( "#gItemDetail" ).animate({
+
+        left: "+=50",
+        height: "toggle"
+      }, 5000, function() {
+        // Animation complete.
+      });   
+      */
+//https://api.jquery.com/animate/
 }
 
+$( document ).ready(function() {
+    $("a.back-to-intro").on("click",onClick_BackToIntro)
+    //console.log( "ready!" );
+});
+
+function onClick_BackToIntro(event){
+    var galleryItem=$(event.target).attr("data-adhiti");
+    console.log(galleryItem);
+    $("#gItemDetail").attr( "style", "display: none;" );
+    $("#gItemIntro").show();
+}
 /**use switch expression to put in all the cases */
